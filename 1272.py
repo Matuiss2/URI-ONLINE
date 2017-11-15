8cb141e6e7583ee8386b29d672545599bf6d67ce
Mateus <31539494+Matuiss2@users.noreply.github.com>
2017-11-15 17:08:55 -0200

    Update 1272.py

--- 1272.py
+++ 1272.py
@@ -2,7 +2,7 @@
 n = int(input())
 for i in range(n):
     a = input().replace("·", " ")  # Troca os pontos por um espaço vazio
-    a = a.split()  # Separa as palavras que estão divididas pelo parametro(nesse caso o espaço vazio)
+    a = a.split()  # Separa as palavras que estão divididas usando o parametro
     texto = ""  # Texto inicial
 # Processo
     for p in a:  # Divide a lista ou string por item(nesse caso cada palavra é um item por causa do split)
