05f065fc923bfc8565faf0d9c90b9dc52b26c939
Mateus <31539494+Matuiss2@users.noreply.github.com>
2017-11-30 11:18:02 -0200

    Update 1133.py

--- 1133.py
+++ 1133.py
@@ -2,4 +2,4 @@ a = int(input())
 b = int(input())
 for i in range(min(a, b) + 1, max(a, b)):  # itera os numeros entre o menor valor e o maior n incluindo eles mesmos.
     if i % 5 == 2 or i % 5 == 3:
-        print(i)  # Imprime quando o numero for dividido por 5 sobrar 2 ou 3
+        print(i)  # Imprime quando o numero for dividido por 5 e sobrar 2 ou 3 depois da operacao
