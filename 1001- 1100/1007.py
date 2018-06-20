03915dd8164ea8330d9c13ed7756cf5e62c97533
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-20 17:09:39 -0300

    Update 1007.py
    remove uma variável desnescessária

--- 1001- 1100/1007.py
+++ 1001- 1100/1007.py
@@ -1,8 +1,5 @@
-n1 = int(input())
-n2 = int(input())
-n3 = int(input())
-n4 = int(input())
-
-diferenca = (n1 * n2) - (n3 * n4)
-
-print("DIFERENCA = %d" %diferenca)
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print("DIFERENCA =", a * b - c * d)
