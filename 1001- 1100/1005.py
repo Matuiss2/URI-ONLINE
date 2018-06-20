1b57a3eb357185b9b890c8da0b6e0e0892a5637a
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-20 17:07:50 -0300

    Update 1005.py
    making the code more concise

--- 1001- 1100/1005.py
+++ 1001- 1100/1005.py
@@ -1,6 +1,3 @@
-a = float(input())
-b = float(input())
-pesoa = a * 3.5
-pesob = b * 7.5
-media = (pesoa + pesob) / 11
-print("MEDIA =", format(media,".5f"))
+n1 = float(input())
+n2 = float(input())
+print("MEDIA = " + format(((n1 * 3.5) + (n2 * 7.5)) / 11, ".5f"))
