f8fe877e7b18499b48d838be6cfc510bd372528c
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-21 04:34:07 -0300

    Update 1042.py
    pequena mudança para deixar mais legível

--- 1001- 1100/1042.py
+++ 1001- 1100/1042.py
@@ -1,6 +1,6 @@
-data = input().split()  # Separa os números e pões em uma lista
-for i in sorted(map(int, data)):  # Transforma os números em int usando map(), e pões em ordem usando sorted()
+data = list(map(int, input().split()))
+for i in sorted(data):
     print(i)
 print()
 for j in data:
-    print(j)  # Não é necessário converter para int aqui
+    print(j)
