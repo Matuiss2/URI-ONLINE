e16d32d2a53293c3e91a4a10efbcc89ba75fb75f
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-21 04:29:58 -0300

    Update 1038.py
    converte todos os valores diretamente do input, e em vez de usar vários if elif usa apenas uma lista

--- 1001- 1100/1038.py
+++ 1001- 1100/1038.py
@@ -1,14 +1,3 @@
-codigo = input().split()
-item = int(codigo[0])
-qnt = int(codigo[1])
-if item == 1:
-    total = qnt * 4.00
-elif item == 2:
-    total = qnt * 4.50
-elif item == 3:
-    total = qnt * 5.00
-elif item == 4:
-    total = qnt * 2.00
-else:
-    total = qnt * 1.50
-print("Total: R$", format(total, ".2f"))
+codigo, quantidade = map(int, input().split())
+tabela_precos = [4, 4.5, 5, 2, 1.5]
+print("Total: R$", format(quantidade * tabela_precos[codigo - 1], ".2f"))
