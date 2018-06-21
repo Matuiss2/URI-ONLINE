b8f3d25db77c554abf7cc6c71b0955370ad51377
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-21 04:40:33 -0300

    Update 1047.py
    muitas melhorias, mas ainda acredito que não está perfeito

--- 1001- 1100/1047.py
+++ 1001- 1100/1047.py
@@ -1,18 +1,14 @@
-# Esse codigo provabelment pode ser reduzido, tem muito codigo repetido
-data = input().split()
-if int(data[2]) <= int(data[0]):
-    horas = int(data[2]) - int(data[0]) + 24   # Se for em outro dia adiciona 24h(um dia) a conta
-    if int(data[3]) < int(data[1]):
-        horas -= 1  # Se for em outra hora remove 1 hora da conta
+hora_inicial, minuto_inicial, hora_final, minuto_final = map(int, input().split())
+if hora_final == hora_inicial == minuto_inicial == minuto_final:
+    horas, minutos = 24, 0
 else:
-    horas = int(data[2]) - int(data[0])
-    if int(data[3]) < int(data[1]):
-        horas -= 1
-if int(data[3]) < int(data[1]):
-    minutos = int(data[3]) - int(data[1]) + 60  # Se for em outra hora adiciona 60 m(1h) a conta
-else:
-    minutos = int(data[3]) - int(data[1])
-
-if horas == 24 and minutos:  # corrige uma falha logica do codigo, que permitia coisas como 24:12(qnd o max é 1 dia)
-    horas -= 24
-print("O JOGO DUROU", horas, "HORA(S) E", minutos, "MINUTO(S)")
+    if minuto_final >= minuto_inicial:
+        minutos = minuto_final - minuto_inicial
+    else:
+        minutos = 60 - abs(minuto_final - minuto_inicial)
+        hora_final -= 1
+    if hora_final >= hora_inicial:
+        horas = hora_final - hora_inicial
+    else:
+        horas = 24 - abs(hora_final - hora_inicial)
+print("O JOGO DUROU {} HORA(S) E {} MINUTO(S)".format(horas, minutos))
