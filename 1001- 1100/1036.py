2e56a378548c8e8a74d08402cbb35aee6d5f751e
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-21 04:28:01 -0300

    Update 1036.py
    remove o import desnescessário e pesado, converte os valores de uma vez só, unifica o print, poderia remover as variáveis r1 e r2 já que elas não se repetem, mas assim fica mais legível

--- 1001- 1100/1036.py
+++ 1001- 1100/1036.py
@@ -1,13 +1,8 @@
-from math import sqrt
-data = input().split()
-a = float(data[0])
-b = float(data[1])
-c = float(data[2])
+a, b, c = map(float, input().split())
 delta = (b * b) - (4 * a * c)
 if delta < 0 or a == 0:
     print("Impossivel calcular")
 else:
-    r1 = (- b + sqrt(delta)) / (2 * a)
-    r2 = (- b - sqrt(delta)) / (2 * a)
-    print("R1 =", format(r1, ".5f"))
-    print("R2 =", format(r2, ".5f"))
+    r1 = (- b + delta ** 0.5) / (2 * a)
+    r2 = (- b - delta ** 0.5) / (2 * a)
+    print("R1 = {}\nR2 = {}".format(format(r1, ".5f"), format(r2, ".5f")))
