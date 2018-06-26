eda72b30e06845a0a794e7eee880aa830bd7664e
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-26 12:56:12 -0300

    Update 1071.py
    mantem a eficiencia, mudando um pouco da escrita do código(juntando variáveis na mesma linha e usando trutsy)

--- 1001- 1100/1071.py
+++ 1001- 1100/1071.py
@@ -1,9 +1,7 @@
-num1 = int(input())
-num2 = int(input())
-maior = max(num1, num2)  # Pega o maior valor entre os 2, para ser o valor final para o for()
-menor = min(num1, num2)  # Pega o menor valor entre os 2, para ser o valor inicial no for()
+numero_inicial, numero_final = int(input()), int(input())
+maior, menor = max(numero_final, numero_inicial), min(numero_final, numero_inicial)
 soma = 0
-for i in range(menor + 1, maior):  # Não pode incluir os próprios números por isso o + 1
-    if i % 2 != 0:
-        soma += i
+for impares in range(menor + 1, maior):
+    if impares % 2:
+        soma += impares
 print(soma)
