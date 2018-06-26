152e191246ca13c361d1544a5e11c720ff5e6e02
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-26 12:50:18 -0300

    Update 1051.py
    remove a repetição de código, mantem a eficiencia

--- 1001- 1100/1051.py
+++ 1001- 1100/1051.py
@@ -1,13 +1,10 @@
-salario = float(input())
-# Todos os val subtraem o salário max anterior e soma o valor máximo possível acumulado nas etapas anteriores
 if 0 < salario <= 2000:
     print("Isento")
-elif 2000 < salario <= 3000:
-    imposto = (salario - 2000) * 0.08
-    print("R$", format(imposto, ".2f"))
-elif 3000 < salario <= 4500:
-    imposto = 80 + (salario - 3000) * 0.18
-    print("R$", format(imposto, ".2f"))
 else:
-    imposto = 350 + (salario - 4500) * 0.28
+    if 2000 < salario <= 3000:
+        imposto = (salario - 2000) * 0.08
+    elif 3000 < salario <= 4500:
+        imposto = 80 + (salario - 3000) * 0.18
+    else:
+        imposto = 350 + (salario - 4500) * 0.28
     print("R$", format(imposto, ".2f"))
