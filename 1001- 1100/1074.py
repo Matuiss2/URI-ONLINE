a3deae652ea339fd4ec4b7da0f66716c733065fc
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-27 07:08:43 -0300

    Update 1074.py
    redução no número de if-elifs, concatenação mais eficiente de strings, remove algumas variáveis aumenta bastante a eficiencia

--- 1001- 1100/1074.py
+++ 1001- 1100/1074.py
@@ -1,17 +1,13 @@
-loops = int(input())
-for i in range(loops):
-    p_i = ""
-    data = int(input())
-    if data == 0:
-        p_i += "NULL"
-    elif data % 2 == 0:
-        p_i += "EVEN " # Adicionar o espaço aqui no final para não dar presentation error
+for i in range(int(input())):
+    numero = int(input())
+    if numero == 0:
+        print("NULL")
     else:
-        p_i += "ODD "  # Adicionar o espaço aqui no final para não dar presentation error
-    if data > 0:
-        p_i += "POSITIVE"
-    elif data < 0:
-        p_i += "NEGATIVE"
-    else:
-        pass
-    print(p_i)
+        if numero % 2:
+            print("ODD", end=" ")
+        else:
+            print("EVEN", end=" ")
+        if numero > 0:
+            print("POSITIVE")
+        else:
+            print("NEGATIVE")
