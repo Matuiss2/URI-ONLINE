2d826f91d649063562127c87cfce5befad11dbc0
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-28 16:18:21 -0300

    Update 1132.py

--- 1101- 1200/1132.py
+++ 1101- 1200/1132.py
@@ -1,8 +1,6 @@
-num1 = int(input())
-num2 = int(input())
-total = 0
-# Menor dos 2 como valor inicial e vai ate o maior o +1 é para incluir o proprio numero)
-for i in range(min(num1, num2), max(num1, num2) + 1):
-    if i % 13 != 0:  # Se não for divisível por 13 entra aqui
+n1, n2, total = int(input()), int(input()), 0
+maior, menor = max(n1, n2), min(n1, n2)
+for i in range(menor, maior + 1):
+    if i % 13:
         total += i
 print(total)
