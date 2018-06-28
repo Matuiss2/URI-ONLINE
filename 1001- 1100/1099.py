0bed49996269d4c07546393a885411c1f0c4d0ee
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-28 04:17:53 -0300

    Update 1099.py
    pequeno aumento na eficiencia e na legibilidade, converte os valores de uma vez.

--- 1001- 1100/1099.py
+++ 1001- 1100/1099.py
@@ -1,10 +1,7 @@
-loops = int(input())
-for i in range(loops):
-    data = input().split()
-    menor = min(map(int,data))  # map converte as (str) para (int), min pega o menor valor(o inicial para o for)
-    maior = max(map(int,data))  # map converte as (str) para (int), min pega o maior valor(o final para o for)
-    total_i = 0
-    for j in range(menor + 1, maior):  # + 1 pois não pode incluir o próprio número
-        if j % 2 != 0:
-            total_i += j  # Acumula todos os ímpares
-    print(total_i)
+for i in range(int(input())):
+    data = map(int, input().split())
+    maior, menor, soma_impares = max(data), min(data), 0
+    for j in range(menor + 1, maior):
+        if j % 2:
+            soma_impares += j
+    print(soma_impares)
