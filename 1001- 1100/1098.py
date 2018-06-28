546c1bcc7ae1bc784ca5d356ab50b95f285371be
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-28 04:15:43 -0300

    Update 1098.py
    aumento na eficiencia e legibilidade do código

--- 1001- 1100/1098.py
+++ 1001- 1100/1098.py
@@ -1,12 +1,5 @@
 i = 0
-j = 0
-for k in range(11):
-    for l in range(1, 4):
-        print("I=" + str(format(i, ".1f")).replace(".0", ""), "J=" + str(format(l + j, ".1f").replace(".0", "")))
-    # Tive que colocar o replace para retirar o .0 dos números para não dar presentation error
-    j += 0.2
+while i <= 2:
+    for j in range(1, 4):
+        print("I={} J={}".format(format(i, ".1f"), format(j + i, ".1f")).replace(".0", ""))
     i += 0.2
-
-
-
-
