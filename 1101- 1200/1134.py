637ec831b99069d44f422eeacd89585e741682d6
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-28 16:22:47 -0300

    Update 1134.py

--- 1101- 1200/1134.py
+++ 1101- 1200/1134.py
@@ -1,19 +1,7 @@
-a = 0  # Esse valores tem que estar fora do loop, ou vão resetar a cada iteração
-g = 0
-d = 0
-while True:
-    data = int(input())
-    while data not in range(1, 5):  # Validação de valor de input, vai ficar pedindo um valor até ele ser um aceitavel
-        data = int(input())
-    if data == 4:  # Se for 4 vai fechar o programa
-        break
-    elif data == 1:
-        a += 1
-    elif data == 2:
-        g += 1
-    else:
-        d += 1
-print("MUITO OBRIGADO")
-print("Alcool:", a)
-print("Gasolina:", g)
-print("Diesel:", d)
+tabela_combustivel, comando = {"1": 0, "2": 0, "3": 0}, 0
+while comando != "4":
+    comando = input()
+    if comando in tabela_combustivel.keys():
+        tabela_combustivel[comando] += 1
+print("MUITO OBRIGADO\nAlcool: {}\nGasolina: {}\nDiesel: {}".format(tabela_combustivel["1"], tabela_combustivel["2"],
+                                                                    tabela_combustivel["3"]))
