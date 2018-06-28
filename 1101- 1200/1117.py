f742af0588c074b6009b372abfd41fe4f351ffaf
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-28 12:37:04 -0300

    Update 1117.py

--- 1101- 1200/1117.py
+++ 1101- 1200/1117.py
@@ -1,12 +1,9 @@
-# Smp q tiver 1 exercício q diz q o último valor é valor vazio(n confundir com 0 ou "") ou valor EOF use esse método
-while True:
-    n1 = float(input())
-    while not 0 <= n1 <= 10:  # Validacao de input vai rodar ate chegar um valor valido(0 a 10)
+corretas, total_notas = 0, 0
+while corretas < 2:
+    nota = float(input())
+    if nota < 0 or nota > 10:
         print("nota invalida")
-        n1 = float(input())
-    n2 = float(input())
-    while not 0 <= n2 <= 10:
-        print("nota invalida")
-        n2 = float(input())
-    print("media =", format((n1 + n2) / 2, ".2f"))
-    break  # Quando os 2 valores forem corretos o programa vai fechar
+    else:
+        corretas += 1
+        total_notas += nota
+print("media =", format(total_notas / 2, ".2f"))
