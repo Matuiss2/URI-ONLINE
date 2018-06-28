cb31f90ce369186e684212d9f52dc5ededfaaecf
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-28 12:30:19 -0300

    Update 1115.py

--- 1101- 1200/1115.py
+++ 1101- 1200/1115.py
@@ -1,15 +1,12 @@
-# Smp q tiver 1 exercício q diz q o último valor é valor vazio(n confundir com 0 ou "") ou valor EOF use esse método
 while True:
-    data = input().split()
-    if int(data[0]) == 0 or int(data[1]) == 0:  # se qq  valor for 0 vai fechar o programa
+    x, y = map(int, input().split())
+    if any(nulo == 0 for nulo in [x, y]):
         break
-    if int(data[0]) > 0:
-        if int(data[1]) > 0:
-            print("primeiro")
-        else:
-            print("quarto")
+    elif all(positivo > 0 for positivo in [x, y]):
+        print("primeiro")
+    elif all(negativo < 0 for negativo in [x, y]):
+        print("terceiro")
+    elif x > 0:
+        print("quarto")
     else:
-        if int(data[1]) > 0:
-            print("segundo")
-        else:
-            print("terceiro")
+        print("segundo")
