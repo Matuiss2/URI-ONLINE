b911e2480d63baa0b7dac0dacd9ad970dc15bf9f
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-29 07:20:30 -0300

    Update 1144.py

--- 1101- 1200/1144.py
+++ 1101- 1200/1144.py
@@ -1,5 +1,3 @@
-loops = int(input()) # Vê a qntd de linhas
-for i in range(1, loops + 1):
-    print(i, i * i, i * i * i)  # o numero inicial  o numero inicial ^ 2 o numero inicial ^ 3
-    for j in range(1):  # a proxima linha sempre sera a primeira mas com o 2° e 3° n° com 1 a mais
-        print(i, i * i + 1, i * i * i + 1)
+for i in range(1, int(input()) + 1):
+    print(i, i * i, i * i * i)
+    print(i, i * i + 1, i * i * i + 1)
