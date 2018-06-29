04b1f1467321eb2c19bb43f456541d59d698c1b8
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-29 07:25:38 -0300

    Update 1146.py

--- 1101- 1200/1146.py
+++ 1101- 1200/1146.py
@@ -1,9 +1,8 @@
-while True:  # O programa vai rodar até chegar no break
+while True:
     numero = int(input())
+    lista_numeros = []
     if numero == 0:
-        break  # se for 0 o programa para
-    final = ""  # string inicial
-    if numero:
-        for i in range(1, numero + 1):
-            final += str(i)+" "  # O método mais eficiente aqui é o "".join(i), mas esse é + conhecido então usei ele
-    print(final[:-1])  # Remove o espaço final 
+        break
+    for i in range(1, numero + 1):
+        lista_numeros.append(str(i))
+    print(" ".join(lista_numeros))
