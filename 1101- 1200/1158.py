db619a922350fbd785c835031ec6e5ec6be59231
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-29 15:32:51 -0300

    Update 1158.py

--- 1101- 1200/1158.py
+++ 1101- 1200/1158.py
@@ -1,10 +1,7 @@
-loops = int(input())
-for i in range(loops):
-    data = input().split()
-    inicial = int(data[0])
-    seq = int(data[1])
-    valor_ini = inicial
-    for j in range(inicial, inicial + (seq * 2)):
-        if j % 2 != 0:
-            valor_ini += j
-    print(valor_ini - inicial)
+for i in range(int(input())):
+    inicial, proximos = map(int, input().split())
+    total = 0
+    for impares in range(inicial, inicial + (proximos * 2)):
+        if impares % 2:
+            total += impares
+    print(total)
