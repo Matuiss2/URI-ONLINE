c38e811a02b78149c68ce90db584de03e3ad65be
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-06-30 04:57:03 -0300

    Update 1160.py

--- 1101- 1200/1160.py
+++ 1101- 1200/1160.py
@@ -1,23 +1,12 @@
-# Entrada
-import math
-T = int(input())
-for i in range(T):
-    numeros = input().split()  # Pega os valores do input e separa
-    PA = int(numeros[0])  # População 1° cidade
-    PB = int(numeros[1])  # População 2° cidade
-    G1 = float(numeros[2])  # Taxa de crescimento da 1° cidade em %
-    G2 = float(numeros[3])  # Taxa de crescimento da 2° cidade em %
-    anos = 0  # Valor inicial para os anos
-# Processo
-    while PA <= PB:  # O exercício diz alcançar e não ultrapassar portanto a condição é <=
-        anos += 1  # Cada iteração aumenta o valor em 1 ano
-        if anos > 100:  # Se durar mais de 1 século para alcançar a população quebra o loop
+for i in range(int(input())):
+    anos = 0
+    populacao1, populacao2, crescimento1, crescimento2 = map(float, input().split())
+    while populacao2 >= populacao1:
+        if anos > 99:
+            print("Mais de 1 seculo.")
             break
-        else:
-            PA += math.floor(PA * G1 / 100)  # arredonda para baixo seguindo as instruções do exercício
-            PB += math.floor(PB * G2 / 100)  # arredonda para baixo seguindo as instruções do exercício
-# Saída
-    if anos > 100:  # Se demorar mais que 100 anos imprime isso
-        print("Mais de 1 seculo.")
+        populacao1 += int(populacao1 * crescimento1 / 100)
+        populacao2 += int(populacao2 * crescimento2 / 100)
+        anos += 1
     else:
-        print(anos, "anos.") 
+        print(anos, "anos.")
